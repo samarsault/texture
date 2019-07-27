259203346601d12dbbfd44735eab2af358c1e856
Samarjeet <thelehhman@gmail.com>
2019-07-28 04:53:44 +0530

    Texture picker hide control

--- assets/js/picker.js
+++ assets/js/picker.js
@@ -5,12 +5,17 @@
 window.onload = function() {
 	// Handle Texture Picker
 	const $picker = document.querySelector('#texture-picker');
+	const $hidePicker = document.querySelector('#hidePicker');
 	const $header = document.querySelector('header');
 	
 	$picker.onclick = function(e) {
 		const textureName = e.target.id;
-		if (textureName && textureName != 'texture-picker') {
+		if (textureName && textureName != 'texture-picker' && textureName != 'hidePicker') {
 			$header.setAttribute('class', `texture-${textureName}`);
 		}
+	}
+
+	$hidePicker.onclick = function () {
+		$picker.style['display'] = 'none';	
 	}
 }
